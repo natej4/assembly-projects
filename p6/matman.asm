37b441688e1037ad939f2c5210cc441ce14f56ce
Nate J <11004247@live.mercer.edu>
2022-11-02 21:25:28 -0400

    finished p6

--- p6/matman.asm
+++ p6/matman.asm
@@ -1,14 +1,17 @@
 ;Nate Jackson
 ;CSC 322 P6
+;11/2/2022
 ;Sum the rows, columns, and all values in a matrix
 
-ROWS:	      EQU    3    ; defines a constant ROWS set to 3.
-COLS:	      EQU    2    ; defines a constant COLS set to 2.
+ROWS:	      EQU    5    ; defines a constant ROWS set to 3.
+COLS:	      EQU    7    ; defines a constant COLS set to 2.
 
 SECTION .data
-MyMatrix:    	dw      1,   2
-		dw      4,   -2
-       		dw     -10,  8
+MyMatrix:    	dw      1,   2, 3, 4, 5, 6, 7
+		dw      8, 9, 10, 11, 12, 13, 14
+       		dw     15, 16, 17, 18, 19, 20, 21
+		dw	22, 23, 24, 25, 26, 27, 28
+		dw	29, 30, 31, 32, 33, 34, 35
 count:	dw 0
 offset: dd 0
 SECTION .bss
@@ -65,6 +68,15 @@ add EBX, 2
 mov WORD [count], 0
 mov AX, 0
 loop topcols
+
+mov EBX, 0
+mov CX, COLS
+
+csum:
+mov AX, [ColSums+ebx]
+add [Sum], AX
+add EBX, 2
+loop csum
 
 lastBreak:
 
